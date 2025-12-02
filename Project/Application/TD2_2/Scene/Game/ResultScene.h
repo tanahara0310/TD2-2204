5fba17215b7b7cbc8f779cbd2510c6c79182a88d
Ratta38 <k024g1030@g.neec.ac.jp>
2025-12-02 15:44:02 +0900

    プレイヤーモデル差し替え

--- Project/Application/TD2_2/Scene/Game/ResultScene.h
+++ Project/Application/TD2_2/Scene/Game/ResultScene.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include "Scene/BaseScene.h"
+#include "Engine/Graphics/Sprite/Sprite.h"
 
 class EngineSystem;
 class CameraManager;
@@ -23,4 +24,22 @@ public:
 	void Finalize() override;
 
 private:
+	// クリア時間　上位3つ
+	float clearTimes_[3]{};
+
+	// 今回のクリアタイム
+	float currentClearTime_ = 0.0f;
+
+	// リザルト画像
+	std::unique_ptr<Sprite> resultSprite_;
+	TextureManager::LoadedTexture resultTexture_;
+
+	// リスタート画像
+	std::unique_ptr<Sprite> restartSprite_;
+
+	// 「タイトルへ」画像
+	std::unique_ptr<Sprite> toTitleSprite_;
+
+	// タイマー画像
+	std::unique_ptr<Sprite> curretTimeSprite_;
 };
